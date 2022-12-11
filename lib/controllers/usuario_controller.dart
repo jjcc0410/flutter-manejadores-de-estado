4ab138d831709507ad69e26352d5fbee5571748c
JoliÃan Camacho Cardoza <jjcamacho0410@gmail.com>
2022-12-11 00:24:20 -0600

    final commit

---
+++ lib/controllers/usuario_controller.dart
@@ -0,0 +1,26 @@
+import '../models/usuario.dart';
+import 'package:get/get.dart';
+
+class UsuarioController extends GetxController {
+  var existeUsuario = false.obs;
+  var usuario = Usuario().obs;
+
+  int get profesionesCount => usuario.value.profesiones.length;
+
+  void cargarUsuario(Usuario pUsuario) {
+    existeUsuario.value = true;
+    usuario.value = pUsuario;
+  }
+
+  void cambiarEdad(int edad) {
+    usuario.update((val) {
+      val!.edad = edad;
+    });
+  }
+
+  void agregarProfesion(String profesion) {
+    usuario.update((val) {
+      val!.profesiones = [...val.profesiones, profesion];
+    });
+  }
+}
